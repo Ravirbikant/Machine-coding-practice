e34d2266e924bd90038ebe7fae08f22497d68452
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-05-06 13:12:33 +0530

    Improved accordion

--- Accordion component/index.js
+++ Accordion component/index.js
@@ -1,13 +1,19 @@
 const accordion = document.querySelectorAll(".accordion");
 const accordionHead = document.querySelectorAll(".accordion-head");
+const icon = document.querySelectorAll(".icon");
 
 let accordionOpen = [];
 
 for (let i = 0; i < accordion.length; i++) {
   accordionOpen.push(false);
   accordionHead[i].addEventListener("click", () => {
-    if (!accordionOpen[i]) accordion[i].style.height = "144px";
-    else accordion[i].style.height = "44px";
+    if (!accordionOpen[i]) {
+      accordion[i].style.height = "144px";
+      icon[i].innerHTML = "&#9650";
+    } else {
+      icon[i].innerHTML = "&#9660";
+      accordion[i].style.height = "44px";
+    }
     accordionOpen[i] = !accordionOpen[i];
   });
 }
